610be9e1fc00e97991974a604bb8e12c50809441
Juan Esteban Cajiao Madero <96344956+BOTHIKIGAI@users.noreply.github.com>
2023-08-05 18:17:39 -0500

    juanes

---
+++ 0-SENA/2 - Actividad-Clase/1-EJERCICIOS/JAVASCRIPT/1-Prestamo.js
@@ -0,0 +1,34 @@
+/*
+
+    Un cliente ha adquirido un crédito de libre inversión por valor de $20.000.000 a un plazo
+    de 3 años, a una tasa de interés mensual de 2.8%. La modalidad de pago es mensual.
+
+    En la cuota nueve, el cliente tarda unos días en cancelar su cuota, por lo que le genera
+    intereses moratorios del 3.9% adicional sobre el valor de la cuota.
+
+    La casa de cobranzas le realiza cobro adicional de 5.3% del valor de la cuota ´vencida por
+    gastos de cobranzas.
+
+    Se desea conocer:
+
+    ● Valor de la amortización a capital que debe pagar en cada cuota
+    ● Valor de intereses causados en la cuota
+    ● Valor total cuota que debe pagar
+    ● Valor a cancelar en la cuota numero 9
+    ● Valor total del pago del crédito, al final de los tres años
+
+    Requerimientos
+
+    1. Valor del credito
+    2. Plazo de tiempo
+    3. Tasa de interes
+    4. Intereses moratorios
+    5. Cobro casa de cobranza
+
+*/
+
+function calcularCobro() {
+
+    
+
+}
