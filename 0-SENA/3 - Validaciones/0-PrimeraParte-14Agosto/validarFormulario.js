db8e76aa686f0f633c90c9531b58064a8151bed9
Juan Esteban Cajiao Madero <96344956+BOTHIKIGAI@users.noreply.github.com>
2023-08-16 14:25:39 -0500

    juanes javascript

--- 0-SENA/3 - Validaciones/0-PrimeraParte-14Agosto/validarFormulario.js
+++ 0-SENA/3 - Validaciones/0-PrimeraParte-14Agosto/validarFormulario.js
@@ -8,11 +8,12 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function validarFormulario() {
         const nombre = document.getElementById("nombre").value;
-        const nombrev = /[a-zA-Záéíóú\s]{3,15}/; // Agrega 'é' e 'í' si es necesario
+        const nombrev = /[a-zA-Záéíóú\s]{3,15}/;
         if (nombrev.test(nombre)) {
             alert("Datos correctos");
             document.getElementById('nombre').classList.remove('formulario_grupo_incorrecto');
-            document.getElementById('nombre').classList.add('formulario_grupo_correcto');
+            document.getElementById('nombre').classList.add('formulario_grupo-correcto');
+            document.querySelector('.formulario_input-error').classList.remove('formulario_input-error-activo');
         } else {
             document.getElementById('nombre').classList.add('formulario_grupo_incorrecto');
             document.querySelector('.formulario_input-error').classList.add('formulario_input-error-activo');
